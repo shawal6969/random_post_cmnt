f8b396cfdb7a2865090490e81d56ee30b9cb279f
shawal6969 <gitudevelopershawal69@gmail.com>
2020-09-05 01:10:46 +0600

    material ui used, post showed, Now next step is linking btn with post details

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Posts from './Components/Posts/Posts';
 
 function App() {
   return (
     <div >
-      
+      <Posts></Posts>
     </div>
   );
 }
